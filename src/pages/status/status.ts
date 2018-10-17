8f5eb53c48a9a764c7ef2bcf60085f9501959085
fityanalimunshi@gmail.com <fityanalimunshi@gmail.com>
2018-10-17 12:12:16 +0700

    getProposal, front-end, showLoader,handler, http-request

--- src/pages/status/status.ts
+++ src/pages/status/status.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { App, MenuController, NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service'
+import { WelcomePage } from '../welcome/welcome'
 /**
  * Generated class for the StatusPage page.
  *
@@ -15,13 +16,57 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class StatusPage {
   subject : any;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  proyekData : any
+  public userDetails : any;
+  public responseData: any;
+  public items : any;
+  loading:any;
+  order_id:any;
+  constructor(public menu: MenuController,public loadingCtrl: LoadingController, public authService:AuthServiceProvider, public app: App, public alertCtrl: AlertController, public navCtrl: NavController, public navParams: NavParams) {
     this.subject= navParams.get('subject');
+    this.order_id = navParams.get('order_id');
+
+    this.menu.swipeEnable(false);
+    const data = JSON.parse(localStorage.getItem('userHiber'));
+    this.userDetails = data;
 
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad StatusPage');
+    this.getProposal();
   }
 
+  showLoader() {
+    this.loading = this.loadingCtrl.create({
+      spinner: 'ios',
+      content: 'Loading..',
+    });
+
+    this.loading.present();
+  }
+
+  backToWelcome(){
+    let nav = this.app.getRootNav();
+    nav.setRoot(WelcomePage);
+   }
+
+  getProposal(){
+    this.showLoader()
+    this.authService.getData('api/user/order_proposal/' + this.order_id, this.userDetails['access_token']).then((result)=>{
+      this.responseData = result;
+      console.log(this.responseData);
+      if(this.responseData['success'] == true){
+        //localStorage.setItem('order_show', JSON.stringify(this.responseData['order']));
+        this.items = this.responseData['data'];
+        this.loading.dismiss()
+      }else{
+        this.loading.dismiss()
+        localStorage.clear();
+        setTimeout(()=> this.backToWelcome(), 1000);  
+      }
+    }, (err) => {
+      this.loading.dismiss()
+    });
+}
 }
